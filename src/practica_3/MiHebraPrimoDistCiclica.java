f1571bc6ba4a3067be009d574225d850a4f8f3f6
Enric-Gil-Gallen <enricgilgallen@gmail.com>
2021-11-02 18:42:22 +0100

    Practica 5 -- Parte de Casa finalizada

--- src/practica_3/MiHebraPrimoDistCiclica.java
+++ src/practica_3/MiHebraPrimoDistCiclica.java
@@ -5,7 +5,6 @@ import static practica_3.EjemploMuestraPrimosEnVector2a.esPrimo;
 public class MiHebraPrimoDistCiclica extends Thread {
     int miId, numHebdas;
     long[] vectorNumeros;
-    private String[] vectorLineas;
 
     public MiHebraPrimoDistCiclica(int miId, int numHebdas, long[] vectorNumeros) {
         this.miId = miId;
